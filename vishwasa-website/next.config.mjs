ca85040c904de7c34f8987dc6e2d0a14bb3231cd
srujankopparam <srujankopparamss@gmail.com>
2026-03-27 19:00:58 +0530

    Add files via upload

--- vishwasa-website/next.config.mjs
+++ vishwasa-website/next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "*.public.blob.vercel-storage.com" },
+    ],
   },
 };
 export default nextConfig;
